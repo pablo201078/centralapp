09f84aa3a7181a334011c7c287e974eeb51ddf7a
pablo201078 <pablo_notti@hotmail.com>
2021-06-14 16:51:30 -0300

    commit

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Pablo Notti on 13/05/2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
